81d109e2412f5e5630ff568f63c56d220844e8e0
Dmitry Tabakerov <github@tabakerov.name>
2021-09-23 19:38:28 +0200

    init

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ReactP5Wrapper } from "react-p5-wrapper";
+
+
+function sketch(p5) {
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+  p5.draw = () => {
+    p5.background(250);
+    p5.normalMaterial();
+    p5.push();
+    p5.rotateZ(p5.frameCount * 0.01);
+    p5.rotateX(p5.frameCount * 0.01);
+    p5.rotateY(p5.frameCount * 0.01);
+    p5.plane(100);
+    p5.pop();
+  };
+}
+
+function Art() {
+  return <ReactP5Wrapper sketch={sketch} />;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Art />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
